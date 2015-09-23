a0656a5b2f12d15e3c628c18c73f02e5a12f754b
b1012216 <b1012216@fun.ac.jp>
2015-09-23 23:32:29 +0900

    新規登録情報をデータベースに入れる処理の追加

--- server.rb
+++ server.rb
@@ -27,7 +27,30 @@ s = WEBrick::HTTPServer.new( config )
 s.config[:MimeTypes]["erb"] = "text/html"
 
 # 処理の登録
+# /signupは新規登録のアクション
+s.mount_proc("/signup") { |req, res|
+
+	p req.query
+
+	# dbhを作成し、データベース'research_log.db'に接続
+	dbh = DBI.connect( 'DBI:SQLite3:research_log.db' )
+
+	# テーブルにデータを追加する
+	dbh.do("insert into users values(null, '#{req.query['username']}', 'takuma.jpg', 25, '#{req.query['firstname']}', '#{req.query['lastname']}', '#{req.query['password']}', '#{req.query['email']}');")
+
+	# データベースとの接続を終了する
+	dbh.disconnect
+
+	# 処理の結果を表示する
+	# ERBを、ERBHandlerを経由せずに直接呼び出して利用している
+	template = ERB.new( File.read('index.erb') )
+	res.body << template.result( binding )
+
+}
+
+# 処理の登録
 # "http://localhost:8099/log"で呼び出される
+# /logは学習を記録するアクション
 s.mount_proc("/log") { |req, res|
 
 	# (注意)本来ならここで入力データに危険や不正がないかチェックするがとりあえず割愛
@@ -49,10 +72,9 @@ s.mount_proc("/log") { |req, res|
 	# 処理の結果を表示する
 	# ERBを、ERBHandlerを経由せずに直接呼び出して利用している
 	template = ERB.new( File.read('index.erb') )
-	res.body << template.result( binding )	
+	res.body << template.result( binding )
 
 }
-
 
 
 #Ctrl-C割り込みがあった場合にサーバーを停止する処理を登録しておく
