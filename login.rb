6cca5edc845ca001c4b30022419825535210c9e1
b1012216 <b1012216@fun.ac.jp>
2015-10-20 14:08:48 +0900

    ログイン時にセッションを張る処理を追加

--- login.rb
+++ login.rb
@@ -11,7 +11,9 @@ require './data_model.rb'
 require 'digest/md5'
 require './my_ruby_library/weather.rb'
 require './my_ruby_library/login_data.rb'
-require "cgi"
+require 'cgi'
+require 'cgi/session'
+
 
 cgi = CGI.new
 
@@ -33,6 +35,32 @@ user.each do |row|
 
       # ログイン成功したのでフラグを1にする
       login_frag = 1
+
+      # 既存のセッションがあるかどうかの確認
+      begin
+        session = CGI::Session.new(cgi,{"new_session"=>false})
+      rescue ArgumentError
+        session = nil
+      end
+
+      #セッションが張られていれば
+      if session != nil
+
+        #セッションは文字列しか保存できないので、数値に変換して1足す
+        session['user_id'] = row.user_id
+        #closeしてセッション情報をサーバに書き込む
+        session.close
+
+      #セッションが張られていなければ
+      else
+
+        #セッションを新規作成してカウント値を初期化
+        session = CGI::Session.new(cgi,{"new_session"=>true})
+        session['user_id'] = row.user_id
+        #closeしてセッション情報をサーバに書き込む
+        session.close
+
+      end
 
       # とりあえず動かすために
       login_user = LoginUser.new
