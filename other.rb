2bb3602d49456561cd1b11fad60c6eba35ed8f02
b1012216 <b1012216@fun.ac.jp>
2015-10-27 10:41:38 +0900

    最終調整

---
+++ other.rb
@@ -0,0 +1,23 @@
+#! /usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+
+require 'erb'
+require 'rubygems'
+require './data_model.rb'
+require 'digest/md5'
+require './my_ruby_library/weather.rb'
+require './my_ruby_library/login_data.rb'
+require 'cgi'
+require 'cgi/session'
+require './heatmap_data.rb'
+
+# CGIを発行
+cgi = CGI.new
+session = CGI::Session.new(cgi)
+
+# 処理の結果を表示する
+# ERBを、ERBHandlerを経由せずに直接呼び出して利用している
+template = ERB.new( File.read('other.erb') )
+puts cgi.header({ 'Content-Type' => 'text/html'})
+print template.result( binding )
