d79de710fd046329fca21b7c5a9fb58004686076
b1012216 <b1012216@fun.ac.jp>
2015-10-29 23:15:44 +0900

    オートログインの処理を追加

---
+++ index_login.rb
@@ -0,0 +1,46 @@
+#! /usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+
+require 'erb'
+require 'rubygems'
+require './data_model.rb'
+require 'digest/md5'
+require 'cgi'
+require 'cgi/session'
+
+# CGIを発行
+cgi = CGI.new
+
+
+# ログイン成功したのでフラグを1にする
+login_frag = 1
+
+# 既存のセッションがあるかどうかの確認
+begin
+  session = CGI::Session.new(cgi,{"new_session"=>false})
+rescue ArgumentError
+  session = nil
+end
+
+#セッションが張られていれば
+if session != nil
+
+  # 処理の結果を表示する
+  # ERBを、ERBHandlerを経由せずに直接呼び出して利用している
+  template = ERB.new( File.read('index.erb') )
+  puts cgi.header({ 'Content-Type' => 'text/html'})
+  print template.result( binding )
+
+else
+
+  # 処理の結果を表示する
+  # ERBを、ERBHandlerを経由せずに直接呼び出して利用している
+  template = ERB.new( File.read('index_login.erb') )
+  puts cgi.header({
+    'Content-Type' => 'text/html',
+    'Location' => cgi['']
+    })
+  print template.result( binding )
+
+end
