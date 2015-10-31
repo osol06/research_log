d61e28e573fbac1b7733b3d4e714c32ebd48280e
b1012216 <b1012216@fun.ac.jp>
2015-11-01 00:25:13 +0900

    ajaxでタイムラインも非同期するようにした

--- log.rb
+++ log.rb
@@ -7,6 +7,7 @@ require 'rubygems'
 require './data_model.rb'
 require 'digest/md5'
 require './my_ruby_library/weather.rb'
+require './database_my_library.rb'
 require 'cgi'
 require 'cgi/session'
 
@@ -18,31 +19,29 @@ session = CGI::Session.new(cgi)
 weather = weather()
 w_frag = weather_frag("#{weather['telop']}")
 
-# 時間が入力されているかの判定
-if(cgi['start_time'].empty? || cgi['finish_time'].empty?)
 
-  # 処理の結果を表示する
-  # ERBを、ERBHandlerを経由せずに直接呼び出して利用している
-  template = ERB.new( File.read('failed_log.erb') )
-  puts cgi.header({ 'Content-Type' => 'text/html'})
-  print template.result( binding )
+# 選択されたタスクidをもってきて
+# カウントを1つ上げる
+task_name = Task_name.find_by(task_name_id: "#{cgi['task_name_id']}", user_id: session['user_id'] )
 
-else
+task_name.update(count: task_name.count + 1)
 
-  # 選択されたタスクidをもってきて
-  # カウントを1つ上げる
-  task_name = Task_name.find_by(task_name_id: "#{cgi['task_name_id']}", user_id: session['user_id'] )
+# テーブルにデータを追加する
+# category_idは一旦保留で1を挿入する事にしている
+Task.create(task_id: nil, user_id: "#{session['user_id']}", category_id: 1, task_name_id: task_name.task_name_id, task_name: "#{task_name.task_name}", start_time: "#{cgi['date']} #{cgi['start_time']}", finish_time: "#{cgi['date']} #{cgi['finish_time']}", group_frag: cgi['group_frag'], comment: "#{cgi['comment']}", music_frag: cgi['music_frag'], weather_frag: w_frag )
 
-  task_name.update(count: task_name.count + 1)
+ajax = {}
 
-  # テーブルにデータを追加する
-  # category_idは一旦保留で1を挿入する事にしている
-  Task.create(task_id: nil, user_id: "#{session['user_id']}", category_id: 1, task_name_id: task_name.task_name_id, task_name: "#{task_name.task_name}", start_time: "#{cgi['date']} #{cgi['start_time']}", finish_time: "#{cgi['date']} #{cgi['finish_time']}", group_frag: cgi['group_frag'], comment: "#{cgi['comment']}", music_frag: cgi['music_frag'], weather_frag: w_frag )
+image_name = User.find_by(user_id: session['user_id'] )
 
-  # 処理の結果を表示する
-  # ERBを、ERBHandlerを経由せずに直接呼び出して利用している
-  template = ERB.new( File.read('index.erb') )
-  puts cgi.header({ 'Content-Type' => 'text/html'})
-  print template.result( binding )
+time = Task.order("task_id desc")
+time = time.select("((UNIX_TIMESTAMP(finish_time) - UNIX_TIMESTAMP(start_time)) / 60) as total")
+time = time.find_by(user_id: session['user_id'])
 
-end
+ajax['image_name'] = image_name
+ajax['task_name'] = task_name.task_name
+ajax['task_time'] = time.total
+ajax['comment'] = cgi['comment']
+
+puts cgi.header({ 'Content-Type' => 'text/html'})
+print "#{image_name.image_name},#{image_name.user_name},#{task_name.task_name},#{time.total.to_i},#{cgi['comment']}"
