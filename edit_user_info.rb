8a96b0bee00761bee411c0ed6930bc4afe86dcb5
b1012216 <b1012216@fun.ac.jp>
2015-11-08 12:54:47 +0900

    卒研日記のリンクを作成

--- edit_user_info.rb
+++ edit_user_info.rb
@@ -10,15 +10,23 @@ require 'cgi/session'
 cgi = CGI.new
 session = CGI::Session.new(cgi)
 
-# サーバ上に画像を保存します。
-open('./images/' + cgi['image_name'].original_filename, "w") {|fh|
-  fh.binmode
-  fh.write cgi['image_name'].read
-}
+if(cgi['image_name'].original_filename != "")
 
-# userのデータを更新
-user = User.find(session['user_id'])
-user.update_attributes(:user_name => "#{cgi['user_name']}", :image_name => "#{cgi['image_name'].original_filename}" )
+  # サーバ上に画像を保存します。
+  open('./images/' + cgi['image_name'].original_filename, "w") {|fh|
+    fh.binmode
+    fh.write cgi['image_name'].read
+  }
+
+  # userのデータを更新
+  user = User.find(session['user_id'])
+  user.update_attributes(:user_name => "#{cgi['user_name']}", :image_name => "#{cgi['image_name'].original_filename}" )
+else
+  # userのデータを更新
+  user = User.find(session['user_id'])
+  user.update_attributes(:user_name => "#{cgi['user_name']}" )
+
+end
 
 # 処理の結果を表示する
 # ERBを、ERBHandlerを経由せずに直接呼び出して利用している
