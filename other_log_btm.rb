e56b101b37f99a20d6175eca3ea4ebd544bc4452
b1012216 <b1012216@fun.ac.jp>
2015-12-11 19:13:19 +0900

    皆の状況可視化をする処理を追加した

---
+++ other_log_btm.rb
@@ -0,0 +1,24 @@
+#! /usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+
+require 'erb'
+require 'rubygems'
+require './data_model.rb'
+require 'digest/md5'
+require './my_ruby_library/weather.rb'
+require './my_ruby_library/login_data.rb'
+require 'cgi'
+require 'cgi/session'
+require './heatmap_data.rb'
+
+# CGIを発行
+cgi = CGI.new
+session = CGI::Session.new(cgi)
+
+# 処理の結果を表示する
+# ERBを、ERBHandlerを経由せずに直接呼び出して利用している
+template = ERB.new( File.read('personal_log.erb') )
+puts cgi.header({ 'Content-Type' => 'text/html'})
+puts cgi["log_user_id"]
+print template.result( binding )
