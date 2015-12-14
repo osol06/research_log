78efb53b80775bdb4620b771e39a501c8bc144a0
b1012216 <b1012216@fun.ac.jp>
2015-12-14 12:29:21 +0900

    時間帯による分散分析のコードを追加

--- t_test.rb
+++ t_test.rb
@@ -78,3 +78,33 @@ RCOMMAND
 
 # Rubyのコード
 puts temperature_r.temperaturetest.to_s
+
+
+
+# 時間帯と学習量の関係
+puts six_to_twelve = timezone_six_to_twelve()
+puts twelve_to_eighteen = timezone_twelve_to_eighteen()
+puts eighteen_to_twentyfour = timezone_eighteen_to_twentyfour()
+
+# ユーザの数
+user_count = user_count()
+# ユーザ名
+user_name = user_name_array()
+p user_name
+
+timezone_result = six_to_twelve.concat(twelve_to_eighteen).concat(eighteen_to_twentyfour)
+
+timezone_r = RSRuby.instance
+
+# ここからRのコード
+puts "時間帯による一元配置分散分析(対応あり)"
+timezone_r.eval_R <<-RCOMMAND
+timezone_result <- c(#{timezone_result.join(",")})
+print(timezone_result)
+zone <- factor(c(rep("6~12",#{user_count}),rep("12~18",#{user_count}),rep("18~24",#{user_count})))
+print(zone)
+person <- factor(rep(c("#{user_name[0]}","#{user_name[1]}","#{user_name[2]}","#{user_name[3]}","#{user_name[4]}","#{user_name[5]}","#{user_name[6]}","#{user_name[7]}"),3))
+print(person)
+result <- summary(aov(timezone_result~zone+person))
+print(result)
+RCOMMAND
